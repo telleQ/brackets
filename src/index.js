12511719782d25852407bfc00ad3d2aa7987a033
telleQ <STEP.nst@gmail.com>
2020-10-14 17:09:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let result = str;
+    // Regular exp.
+    // config1 = \(\) = ()
+    //         = \[\] = []
+    //         = \{\} = {}
+    // config4 = \|\| = ||
+    // config6 = 12 or 34 or 56 or 77 or 88
+    const reg = /\(\)|\[\]|\{\}|\|\||12|34|56|77|88/g;
+    for (let i = 0; i < str.length; i++) {
+        result = result.replace(reg, '');
+    };
+    if (result.length > 0) {
+        return false;
+    };
+    return true;
+};
